50a42b764b8b8cf0fab358b1392a6dac500c8478
David Louvton <dlouvton@salesforce.com>
2020-07-14 13:35:20 -0700

    new-api

--- src/main/index.js
+++ src/main/index.js
@@ -37,13 +37,13 @@ crossWindowTelephonyEventTypes.forEach((eventType) => {
 function channelMessageHandler(message) {
     switch (message.data.type) {
         case constants.MESSAGE_TYPE.ACCEPT_CALL:
-            vendorAdapter().acceptCall();
+            vendorAdapter().acceptCall(message.data.call);
             break;
         case constants.MESSAGE_TYPE.DECLINE_CALL:
-            vendorAdapter().declineCall();
+            vendorAdapter().declineCall(message.data.call);
             break;
         case constants.MESSAGE_TYPE.END_CALL:
-            vendorAdapter().endCall(message.data.participant, message.data.agentStatus);
+            vendorAdapter().endCall(message.data.call, message.data.agentStatus);
             break;
         case constants.MESSAGE_TYPE.MUTE:
             vendorAdapter().mute();
@@ -52,10 +52,10 @@ function channelMessageHandler(message) {
             vendorAdapter().unmute();
             break;
         case constants.MESSAGE_TYPE.HOLD:
-            vendorAdapter().hold(message.data.participant);
+            vendorAdapter().hold(message.data.call);
             break;
         case constants.MESSAGE_TYPE.RESUME:
-            vendorAdapter().resume(message.data.participant);
+            vendorAdapter().resume(message.data.call);
             break;
         case constants.MESSAGE_TYPE.SET_AGENT_STATUS:
             vendorAdapter().setAgentStatus(message.data.agentStatus);
@@ -70,10 +70,10 @@ function channelMessageHandler(message) {
             vendorAdapter().getPhoneContacts();
             break;
         case constants.MESSAGE_TYPE.SWAP_PARTICIPANTS:
-            vendorAdapter().swapCallParticipants();
+            vendorAdapter().swapCallParticipants(message.data.calls);
             break;
         case constants.MESSAGE_TYPE.JOIN_PARTICIPANTS:
-            vendorAdapter().joinCallParticipants();
+            vendorAdapter().joinCallParticipants(message.data.calls);
             break;
         case constants.MESSAGE_TYPE.TRANSFER:
             vendorAdapter().transfer(message.data.destination);
@@ -85,10 +85,10 @@ function channelMessageHandler(message) {
             vendorAdapter().login(message.data.credentials);
             break;
         case constants.MESSAGE_TYPE.PAUSE_RECORDING:
-            vendorAdapter().pauseRecording();
+            vendorAdapter().pauseRecording(message.data.call);
             break;
         case constants.MESSAGE_TYPE.RESUME_RECORDING:
-            vendorAdapter().resumeRecording();
+            vendorAdapter().resumeRecording(message.data.call);
             break;
         default:
             break;
@@ -102,7 +102,7 @@ function windowMessageHandler(message) {
         case constants.MESSAGE_TYPE.SETUP_CONNECTOR:
             channelPort = message.ports[0];
             channelPort.onmessage = channelMessageHandler;
-            vendorAdapter().init(message.data.connectorConfig);
+            vendorAdapter().init(message.data.connectorConfig, message.data.container);
             window.removeEventListener('message', windowMessageHandler);
             break;
         default:
@@ -170,3 +170,116 @@ export function setConnectorReady() {
 
 export const Constants = constants;
 export const CrossWindowTelephonyEventTypes = crossWindowTelephonyEventTypes;
+
+/**
+ * @typedef {"PhoneBook" | "Queue" | "PhoneNumber"} ContactType
+ */
+
+/** 
+ * Class representing a CallTarget. This object is used to represent 
+ * phone system contact or an outbound call target
+ */
+
+export class CallTarget {
+
+    /**
+     * Create a CallTarget.
+     * @param {String} id - The unique contactId
+     * @param {ContactType} contactType - The type of the contact
+     * @param {String} label - The label for this contact to be displayed in the UI
+     * @param {String} voiceCallId - The Salesforce Id of the VoiceCall Object
+     * @param {String} phoneNumber - The phone number associcated with this contact
+     * @param {String} prefix - Any preix to be dialed before dialing the number
+     * @param {String} extension - Any extenstion to be dialed after dialing the number
+     * 
+     */
+    constructor(id, type, label, phoneNumber, prefix, extension) {
+        this.contactId = id;
+        this.contactType = type;
+        this.label = label;
+        this.phoneNumber = phoneNumber;
+        this.prefix = prefix; 
+        this.extension = extension;
+    }
+}
+
+/**
+ * @typedef {"Inbound" | "Outbound" | "Transfer"} CallType
+ */
+
+/** 
+* Class representing a PhoneCall. 
+*/
+
+export class PhoneCall {
+    /**
+     * Create a PhoneCall.
+     * @param {String} callId - The unique callId. This is a required parameter
+     * @param {CallType} callType - The type of the call
+     * @param {String} callState - The state of the call
+     * @param {String} voiceCallId - The Salesforce Id of the VoiceCall Object
+     * @param {String} phoneNumber - The phone number of the other party
+     * @param {Object} callAttributes - Any additional call attributes 
+     * @param {string} callAttributes.reason - The reason in case the call is ended
+     * @param {string} callAttributes.parentId - The parent Id of this tis call in case of transfer
+     */
+    constructor(callId, callType, state, voiceCallId, phoneNumber, callAttributes) {
+        this.callType = callType;
+        this.callId = callId;
+        this.state = state;
+        this.voiceCallId = voiceCallId;
+        this.phoneNumber = phoneNumber;
+        this.callAttributes = callAttributes;
+    }
+}
+
+/**
+ * @typedef {"AlphaNumeric" | "String" | "Number" | "Boolean" | "Enum" } FieldType
+ */
+
+ /** Class representing a single login field */
+ export class LoginField {
+    /**
+     * Create a Login Field
+     * @param {String} fieldId - Unique Id of the field 
+     * @param {String} fieldLabel - The label of the field to be shown in the UI
+     * @param {FieldType} fieldType - The type of field. This will deterine what UI control to show in the UI
+     * @param {fieldValues} fieldValues - Possible values to choose from in case of an Enum field type
+     */
+    constructor(fieldId, fieldLabel, fieldType, fieldValues) {
+        this.fieldId = fieldId;
+        this.fieldLabel = fieldLabel;
+        this.fieldType = fieldType;
+        this.fieldValues = fieldValues;
+    }
+ }
+
+ /** Class representing the telephone system branding info- This will be shown in the UI */
+ export class TelephonySystemInfo {
+    /**
+     * Create a TelephonySystemInfo
+     * @param {String} companyName - Company name of the telephony System.
+     * @param {String} productName - Product name of the telephony system
+     * @param {String} brandingAsset - base64 encoded Image of the telephony system branding asset for the UI
+     */
+    constructor(companyName, productName, brandingAsset) {
+        this.companyName = companyName;
+        this.productName = productName;
+        this.brandingAsset = brandingAsset;
+    }
+ }
+
+/** Class representing Login Settings */
+export class LoginSettings {
+    /**
+     * Create a LoginSettings.
+     * @param {Boolean} loginRequired - Boolean regarding whether the login is required or not. 
+     * @param {LoginField[]} loginFields - Ordered list of login fields to show in the UI
+     * @param {TelephonySystemInfo} telephonsystemInfo - Telephony System Info for UI branding
+     */
+    constructor(loginRequired, loginFields, telephonsystemInfo) {
+        this.loginRequired = loginRequired;
+        this.loginFields = loginFields;
+        this.telephonsystemInfo = telephonsystemInfo;
+    }
+}
