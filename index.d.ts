526b82840cb4d52c3762d69741e0a66f6a383369
Raghav Janamanchi <rjanamanchi@salesforce.com>
2020-10-15 16:43:14 -0700

    Updating index.d.ts file

--- index.d.ts
+++ index.d.ts
@@ -1,83 +1,15 @@
 export function initializeConnector(connector: getConnector): void;
-export function setConnectorReady(): void;
-export function isConnectorReady(): boolean;
-export function dispatchEvent(eventType: string, payload: object): void;
-export function dispatchError(errorType: string, optionalError: string): void;
+export async function publishEvent(eventType: Constants.EVENT_TYPE, payload: object): void;
 export const Constants: {
-    MESSAGE_TYPE: {
-        SETUP_CONNECTOR: string;
-        CONNECTOR_READY: string;
-        TELEPHONY_EVENT_DISPATCHED: string;
-        ACCEPT_CALL: string;
-        DECLINE_CALL: string;
-        END_CALL: string;
-        MUTE: string;
-        UNMUTE: string;
-        HOLD: string;
-        RESUME: string;
-        SET_AGENT_STATUS: string;
-        DIAL: string;
-        SEND_DIGITS: string;
-        GET_PHONE_CONTACTS: string;
-        SWAP_PARTICIPANTS: string;
-        ADD_PARTICIPANT: string;
-        CONFERENCE: string;
-        PAUSE_RECORDING: string;
-        RESUME_RECORDING: string;
-        LOGOUT: string;
-        GET_CAPABILITIES: string;
-    };
     EVENT_TYPE: {
         CALL_STARTED: string;
         CALL_CONNECTED: string;
-        CALL_FAILED: string;
-        MUTE_TOGGLE: string;
-        HOLD_TOGGLE: string;
-        HANGUP: string;
-        ERROR: string;
-        PHONE_CONTACTS: string;
-        PARTICIPANT_ADDED: string;
         PARTICIPANT_CONNECTED: string;
         PARTICIPANT_REMOVED: string;
-        LOGIN_STARTED: string;
         LOGIN_RESULT: string;
         LOGOUT_RESULT: string;
-        RECORDING_TOGGLE: string;
-        CAPABILITIES: string;
-        SHOW_LOGIN: string;
-        SET_AGENT_STATUS_RESULT: string;
-        WRAP_UP_ENDED: string;
-    };
-    ERROR_TYPE: {
-        GENERIC_ERROR: string;
-        AGENT_ERROR: string;
-        MICROPHONE_NOT_SHARED: string;
-        UNSUPPORTED_BROWSER: string;
-        AUTHENTICATION_ERROR: string;
-        INVALID_AGENT_STATUS: string;
-        CAN_NOT_SET_AGENT_STATUS: string;
-        LOGIN_REQUIRED: string;
-        CAN_NOT_ACCEPT_THE_CALL: string;
-        CAN_NOT_HOLD_CALL: string;
-        CAN_NOT_RESUME_CALL: string;
-        CAN_NOT_MUTE_CALL: string;
-        CAN_NOT_UNMUTE_CALL: string;
-        INVALID_PARTICIPANT: string;
-        CAN_NOT_LOG_IN: string;
-        CAN_NOT_LOG_OUT: string;
-        INVALID_PARAMS: string;
-        CAN_NOT_GET_PHONE_CONTACTS: string;
-        CAN_NOT_SWAP_PARTICIPANTS: string;
-        CAN_NOT_CONFERENCE: string;
-        INVALID_DESTINATION: string;
-        INVALID_PHONE_NUMBER: string;
-        CAN_NOT_HANGUP_PARTICIPANT: string;
-        CAN_NOT_ADD_PARTICIPANT: string;
-        CAN_NOT_START_THE_CALL: string;
-        CAN_NOT_GET_LOGIN_SETTINGS: string;
-        CAN_NOT_PAUSE_RECORDING: string;
-        CAN_NOT_RESUME_RECORDING: string;
-        CAN_NOT_GET_CAPABILITIES: string;
+        /* This is only added to aid in connector development. This will be removed before publishing it*/
+        REMOTE_CONTROLLER: string;
     };
     AGENT_STATUS: {
         ONLINE: string;
@@ -102,12 +34,6 @@ export const Constants: {
         PHONEBOOK: string;
         QUEUE: string;
         PHONENUMBER: string;
-    };
-    CAPABILITY_TYPE: {
-        SHOW_AGENT_SETTINGS: string;
-        RECORD: string;
-        MUTE: string;
-        HOLD: string;
     };
 };
 export class Contact {
@@ -142,22 +68,79 @@ export type ContactParam = {
     prefix?: string,
     extension?: string
 }
+export type CapabilitiesResult = {
+    hasMute?: boolean,
+    hasHold?: boolean,
+    hasRecord?: boolean,
+    hasMerge?: boolean
+}
+export type ActiveCallsResult = {
+    activeCalls: PhoneCall[]
+}
+export type MuteToggleResult = {
+    isMuted: boolean
+}
+export type RecordingToggleResult = {
+    isRecordingPaused: boolean,
+    contactId?: string,
+    initialContactId?: string,
+    instanceId?: string,
+    region?: string
+}
+export type ParticipantRemovedResult = {
+    reason: string
+}
+export type CallInfo = {
+    isOnHold: boolean,
+    callStateTimestamp?: Date
+}
+export type ParticipantResult = {
+    initialCallHasEnded: boolean,
+    callInfo: CallInfo,
+    phoneNumber: string
+}
+export type ConferenceResult = {
+    isThirdPartyOnHold: true,
+    isCustomerOnHold: true
+}
+export type PhoneContactsResult = {
+    contacts: Contact[]
+}
+export type CallResult = {
+    call: PhoneCall
+}
+export type HoldToggleResult = {
+    isThirdPartyOnHold: boolean,
+    isCustomerOnHold: boolean,
+    calls: PhoneCall[]
+}
+export type InitResult = {
+    showLogin?: boolean,
+    loginFrameHeight?: number
+}
+export type GenericResult = {
+    success: boolean
+}
 export type getConnector = () => Connector;
 export interface Connector {
-    init(callCenterConfig: object): void;
-    acceptCall(call: PhoneCall): void;
-    declineCall(call: PhoneCall): void;
-    endCall(call: PhoneCall): void;
-    mute(): void;
-    unmute(): void;
-    hold(call: PhoneCall): void;
-    resume(call: PhoneCall): void;
-    pauseRecording(call: PhoneCall): void;
-    resumeRecording(call: PhoneCall): void;
-    swap(call1: PhoneCall, call2: PhoneCall): void;
-    conference(calls: PhoneCall[]): void;
-    dial(contact: Contact): void;
+    init(callCenterConfig: object): Promise<CallResult>;
+    acceptCall(call: PhoneCall): Promise<CallResult>;
+    declineCall(call: PhoneCall): Promise<CallResult>;
+    endCall(call: PhoneCall): Promise<CallResult>;
+    mute(): Promise<MuteToggleResult>;
+    unmute(): Promise<MuteToggleResult>;
+    hold(call: PhoneCall): Promise<HoldToggleResult>;
+    resume(call: PhoneCall): Promise<HoldToggleResult>;
+    pauseRecording(call: PhoneCall): Promise<RecordingToggleResult>;
+    resumeRecording(call: PhoneCall): Promise<RecordingToggleResult>;
+    swap(call1: PhoneCall, call2: PhoneCall): Promise<HoldToggleResult>;
+    conference(calls: PhoneCall[]): Promise<ConferenceResult>;
+    dial(contact: Contact): Promise<CallResult>;
     sendDigits(digits: string): void;
-    getPhoneContacts(): void;
-    addParticipant(contact: Contact, call: PhoneCall): void;
+    setAgentStatus(agentStatus: string): Promise<GenericResult>;
+    getPhoneContacts(): Promise<PhoneContactsResult>;
+    addParticipant(contact: Contact, call: PhoneCall): Promise<ParticipantResult>;
+    logout(): Promise<GenericResult>;
+    handleMessage(message: object): void;
+    getCapabilities(): Promise<CapabilitiesResult>
 }
